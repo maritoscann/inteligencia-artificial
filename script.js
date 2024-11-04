d1ac36ebf01604994af5e6fd45f9587c9a9337b0
maritoscann <110538965+maritoscann@users.noreply.github.com>
2024-11-04 08:57:04 -0300

    criação de perguntas

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa__principal");
+const caixaPerguntas = document.querySelector(".caixa__perguntas");
+const caixaAlternativas = document.querySelector(".caixa__alternativas");
+const caixaResultados = document.querySelector(".caixa__resultados");
+const textoResultado = document.querySelector(".texto__resultado");
+
+const perguntas = [ 
+    {
+    enunciado: "Pergunta 1"
+    alternativas: [ 
+        "Alternativa 1"; 
+        "Alternativa 2"
+    ]
+    },
+    {
+        enunciado: "Pergunta 1"
+        alternativas: [ 
+            "Alternativa 1"; 
+            "Alternativa 2"
+        ]
+        },
+];
